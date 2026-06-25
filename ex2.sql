9d9f69b2da14a483011243774c3b9afd5131dfec
nguyendamngocnhutgdd2004-boop <nguyendamngocnhutgdd2004@gmail.com>
2026-06-25 10:13:39 +0900

    ex2

---
+++ ex2.sql
@@ -0,0 +1,5 @@
+CREATE TABLE MonHoc (
+    MaMon VARCHAR(10) PRIMARY KEY,      -- Mã môn học 
+    TenMon VARCHAR(50) NOT NULL,        -- Tên môn học 
+    SoTinChi INT NOT NULL               -- Số tín chỉ 
+);
---
+++ vd2.sql
@@ -0,0 +1,5 @@
+INSERT INTO MonHoc (MaMon, TenMon, SoTinChi) 
+VALUES ('CS101', 'Co so du lieu', 3);
+
+-- Xem lại bảng môn học
+SELECT * FROM MonHoc;
